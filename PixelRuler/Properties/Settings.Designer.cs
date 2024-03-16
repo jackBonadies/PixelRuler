84b3b2a1021688fbb1058f037e3a4b351eec5cc3
jackBonadies <jbonadies6@gmail.com>
2024-03-16 11:25:38 -0400

    better defaults

--- PixelRuler/Properties/Settings.Designer.cs
+++ PixelRuler/Properties/Settings.Designer.cs
@@ -73,7 +73,7 @@ namespace PixelRuler.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool GlobalShortcutsEnabled {
             get {
                 return ((bool)(this["GlobalShortcutsEnabled"]));
@@ -157,7 +157,7 @@ namespace PixelRuler.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("66")]
+        [global::System.Configuration.DefaultSettingValueAttribute("6")]
         public int GlobalShortcutWindowModifiers {
             get {
                 return ((int)(this["GlobalShortcutWindowModifiers"]));
